d2e79ab01b69da09fbab8d5d14d74073ff287ff6
phadoranym <57186053+phadoranym@users.noreply.github.com>
2021-01-26 12:55:55 +0800

    yes
    yes

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import { IConfig } from "terraria-server";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const CONFIG: IConfig = {
+    SERVER_URL:
+        "https://terraria.org/system/dedicated_servers/archives/000/000/042/original/terraria-server-1412.zip", // Terraria official server 1.4.1.2
+    BUILD_DIRECTORY: "./server",
+    FILE_NAME: "server.zip",
+    PORT: parseInt(process.env.PORT as string) || 3000,
+    SERVER_CONFIG: {
+        WORLDS_FOLDER: "./server/worlds",
+        WORLD_FILE: "MyWorld",
+        BAN_LIST: "banlist",
+        MOTD: "Welcome to Terraria!",
+        PORT: 7777,
+        PASSWORD: "p@55w0rd",
+        MAX_PLAYERS: 2,
+        LANGUAGE: "en-US",
+        DIFFICULTY: 0,
+        WORLD_SIZE: 1,
+    },
+};
+
+export default CONFIG;
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import build from "./build";
+import start from "./start";
+import parser from "command-line-args";
+import CONFIG from "./config";
+import "colors";
+
+const definitions = [
+    { name: "build", alias: "b", type: Boolean },
+    { name: "start", alias: "s", type: Boolean },
+];
+
+const args = parser(definitions, {
+    partial: true,
+});
+
+if (args.build && !args.start) {
+    // Build and no start
+    build(CONFIG);
+} else if (!args.build && args.start) {
+    // Start and no build
+    start(CONFIG);
+} else {
+    console.info(
+        `${"[TerrariaServer]".bgRed.black}: ${"Available options".green} -> ${
+            "--build, --start".yellow
+        }`,
+    );
+}
